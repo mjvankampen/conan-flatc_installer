f1549e8f95c42e47fcd942c62fa1ec775aa7f104
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-07 14:03:18 +0200

    CI fixes
    Added build policy missing to CI tooling

--- .ci/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-conan install flatbuffers/1.11.0@google/stable --build flatbuffers
--- build.py
+++ build.py
@@ -8,7 +8,6 @@ from conans import tools
 
 
 if __name__ == "__main__":
-    docker_entry_script = ".ci/build.sh" if tools.os_info.is_linux else None
     arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder(docker_entry_script=docker_entry_script)
     builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
